6e57010c14632fa269fb071feaa920026e5c68e9
Akula-jyothi901 <akulajyothifeb21@gamil.com>
2024-12-02 11:21:20 +0530

    all

---
+++ TestWork/test.py
@@ -0,0 +1,8 @@
+class Calculator:
+    def add(self, a, b):
+        return a+b
+    def subtract(self, a, b):
+        return a-b
+Cal = Calculator()
+add_result = Cal.add(10,39)
+print(add_result)
